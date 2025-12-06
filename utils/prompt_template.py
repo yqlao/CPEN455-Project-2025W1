77e3a1c123dff14e4fa5a28e89343a828485c922
Yuqi Lao <blakelao77@gmail.com>
2025-12-06 14:58:35 -0800

    change prompt cutoff

---
+++ utils/prompt_template.py
@@ -0,0 +1,20 @@
+PROMPT_TEMPLATE = (
+    "{user_prompt}\n"
+    "The following email is labeled as {label}.\n"
+    "Subject: {subject}\n"
+    "Message: {message}"
+)
+
+def get_prompt(subject: str, message: str, label: str, max_seq_length: int = 256, user_prompt: str = "") -> str:
+    prompt = PROMPT_TEMPLATE.format(user_prompt=user_prompt, subject=subject, message=message, label=label)
+    prompt = prompt[:max_seq_length*4]
+    return prompt
+
+if __name__ == "__main__":
+
+    print(get_prompt(
+        user_prompt="Please classify the email below as ham or spam.",
+        subject="Hello", 
+        message="This is a test message.", 
+        label="ham", 
+        max_seq_length=256))
